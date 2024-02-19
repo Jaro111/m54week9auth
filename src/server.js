5baa914eaf4b5a41d01b51c64232f48774cf9695
Jaro111 <jarokark@protonmail.com>
2024-02-19 10:39:06 +0000

    enhancement: add basic server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
