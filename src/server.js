549fe49205415fa0a10851b1086320454c1af4a3
Jaro111 <jarokark@protonmail.com>
2024-02-19 14:06:00 +0000

    enhancement: user routes add

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 
+const User = require("./users/model");
+const userRouter = require("./users/routes");
+
 const app = express();
 
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
+app.use(userRouter);
+
+const SyncTables = () => {
+  User.sync();
+};
+
 app.listen(port, () => {
+  SyncTables();
   console.log(`Server is listening on ${port}`);
 });
---
+++ src/users/controlers.js
@@ -0,0 +1,29 @@
+const User = require("./model");
+
+const signupuUser = async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    //
+    res.status(201).json({ message: `user created`, user: user });
+  } catch (error) {
+    res.status(501).json({ message: error.message, error: error });
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(201).json({ message: `Users uploaded`, users: users });
+  } catch (error) {
+    res.status(501).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = {
+  signupuUser: signupuUser,
+  getUsers: getUsers,
+};
